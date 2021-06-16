771c14b449db786da31b0aaaf6d20078fc6b9c83
mayu <63080020+mayu-snba19@users.noreply.github.com>
2021-06-16 11:09:31 +0900

    change malloc

--- week6/real_malloc/malloc.c
+++ week6/real_malloc/malloc.c
@@ -14,8 +14,7 @@
 //      challenge, |size| is guaranteed to be a multiple of 8 bytes and meets
 //      8 <= size <= 4000.
 //   * my_free(ptr) is called every time an object is freed.
-//   * Additionally, my_finalize() is called only once at the end of each
-//   challenge,
+//   * Additionally, my_finalize() is called only once at the end of each challenge,
 //     so you can use this function for doing some clean ups if you want.
 // 2. The only library functions you can use in my_malloc() and my_free() are
 //    mmap_from_system() and munmap_to_system().
@@ -66,9 +65,42 @@
 void *mmap_from_system(size_t size);
 void munmap_to_system(void *ptr, size_t size);
 
+typedef struct metadata_t {
+  size_t size;
+  struct metadata_t *next;
+} metadata_t;
+
+typedef struct heap_t {
+  metadata_t *free_head;
+  metadata_t dummy;
+} heap_t;
+
+heap_t heap;
+
+// Add a free slot to the beginning of the free list.
+void add_to_free_list(metadata_t *metadata) {
+  assert(!metadata->next);
+  metadata->next = heap.free_head;
+  heap.free_head = metadata;
+}
+
+// Remove a free slot from the free list.
+void remove_from_free_list(metadata_t *metadata, metadata_t *prev) {
+  if (prev) {
+    prev->next = metadata->next;
+  } else {
+    heap.free_head = metadata->next;
+  }
+  metadata->next = NULL;
+}
+
+
 // my_initialize() is called only once at the beginning of each challenge.
 void my_initialize() {
   // Implement here!
+  heap.free_head = &heap.dummy;
+  heap.dummy.size = 0;
+  heap.dummy.next = NULL;
 }
 
 // my_malloc() is called every time an object is allocated. |size| is guaranteed
@@ -77,14 +109,42 @@ void my_initialize() {
 // munmap_to_system.
 void *my_malloc(size_t size) {
   // Implement here!
-  return mmap_from_system(4096);
+  metadata_t *metadata = heap.free_head;
+  metadata_t *prev = NULL;
+  while (metadata && metadata->size < size) {
+    prev = metadata;
+    metadata = metadata->next;
+  }
+
+  if (!metadata) {
+    size_t buffer_size = 4096;
+    metadata_t *metadata =
+        (metadata_t *)mmap_from_system(buffer_size);
+    metadata->size = buffer_size - sizeof(metadata_t);
+    metadata->next = NULL;
+    add_to_free_list(metadata);
+    return my_malloc(size);
+  }
+  void *ptr = metadata + 1;
+  size_t remaining_size = metadata->size - size;
+  metadata->size = size;
+  remove_from_free_list(metadata, prev);
+
+  if (remaining_size > sizeof(metadata_t)) {
+    metadata_t *new_metadata = (metadata_t *)((char *)ptr + size);
+    new_metadata->size = remaining_size - sizeof(metadata_t);
+    new_metadata->next = NULL;
+    add_to_free_list(new_metadata);
+  }
+  return ptr;
 }
 
 // my_free() is called every time an object is freed.  You are not allowed to
 // use any library functions other than mmap_from_system / munmap_to_system.
 void my_free(void *ptr) {
   // Implement here!
-  munmap_to_system(ptr, 4096);
+  metadata_t *metadata = (metadata_t *)ptr - 1;
+  add_to_free_list(metadata);
 }
 
 void my_finalize() {
